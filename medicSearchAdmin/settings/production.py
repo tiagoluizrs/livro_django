2f485bcd130be2475154b92b1892ed153c960383
tiagoribeirocb <tiago.ribeiro@codebuddy.com.br>
2020-07-20 15:39:40 -0300

    make it better

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,19 @@
+from .settings import *
+
+DEBUG = True
+
+# Crie a secret key para seu ambiente de desenvolvimento
+SECRET_KEY = 'ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
+
+ALLOWED_HOSTS = ['livro-django.herokuapp.com']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
