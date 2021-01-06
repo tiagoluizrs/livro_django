863e55ada8d259038de020c70a5ce1cd4fc8e62e
Tiago Silva <tiagoluizrs@MacBook-Air-de-Tiago.local>
2021-01-06 11:53:06 -0300

    correcoes

--- medicSearch/views/ProfileView.py
+++ medicSearch/views/ProfileView.py
@@ -37,21 +37,28 @@ def list_profile_view(request, id=None):
 def edit_profile(request):
     profile = get_object_or_404(Profile, user=request.user)
     message = None
+    emailUnused = True
 
     if request.method == 'POST':
         profileForm = UserProfileForm(request.POST, request.FILES,instance=profile)
         userForm = UserForm(request.POST, instance=request.user)
+        
+        verifyEmail = Profile.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
     else:
         profileForm = UserProfileForm(instance=profile)
         userForm = UserForm(instance=request.user)
 
-    if profileForm.is_valid() and userForm.is_valid():
+    if profileForm.is_valid() and userForm.is_valid() and emailUnused:
         profileForm.save()
         userForm.save()
         message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
     else:
         if request.method == 'POST':
-            message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            if emailUnused:
+                message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            else:   
+                message = { 'type': 'warning', 'text': 'E-mail já usado por outro usuário' }
 
     context = {
         'profileForm': profileForm,
